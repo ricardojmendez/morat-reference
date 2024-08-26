d0aef99e8f6c7006305b55baf5aa45b94619b81f
Ricardo J. Méndez <ricardo@numergent.com>
2024-08-26 14:37:48 +0200

    User registration

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,87 @@ import { Elysia, t } from 'elysia';
 
 /**
  * Basic, trivial prototype to play with the points concept.
- * 
+ *
  * OK, things to do here...
- * 
+ *
  * [x] Tick a new epoch
- * [ ] Allow registration of a new user
- * [ ] Every new user gets 1k points per epoch
+ * [x] Allow registration of a new user
+ * [x] Every new user gets 1k points per epoch
  * [ ] Allow a user to transfer points to another user
  * [ ] Allow querying of current user points
  * [ ] User point refill on epoch tick
- * 
+ *
  * We'll do all of these in memory for now. This is a prototype.
- * 
+ *
  * If this is going to end up running on Solana, it will likely need account
  * compression in order to save on storage costs.
- * 
+ *
  */
 
 let currentEpoch = 0;
 
-const users: Map<number, { name: string; address: string }> = new Map([
-	[1, { name: 'Joe', address: '1234' }],
-	[2, { name: 'Jane', address: '5678' }],
-]);
+export type User = {
+	key: string;
+	epochSignUp: number;
+	currentPoints: number;
+	createDate: number;
+	timestamp: number;
+};
+
+const users: Map<string, User> = new Map();
 
 const app = new Elysia()
 	.get('/', () => 'Hello Elysia')
-	.get('/user/:id', ({ params: { id } }) => users.get(id), {
-		params: t.Object({
-			id: t.Numeric(),
-		}),
-	})
-    .get('/epoch', () => currentEpoch)
-    .post('/epoch/tick', () => currentEpoch++)
+	.get(
+		'/user/:id',
+		({ params: { id }, error }) => {
+			if (!users.has(id)) {
+				return error(404, 'User not found');
+			} else {
+				return users.get(id);
+			}
+		},
+		{
+			params: t.Object({
+				id: t.String(),
+			}),
+		}
+	)
+	.post(
+		'/user/:id',
+		({ params: { id }, error }) => {
+			if (users.has(id)) {
+				return error(409, 'User already exists');
+			} else {
+				users.set(id, {
+					key: id,
+					epochSignUp: currentEpoch,
+					currentPoints: 1000,
+					createDate: Date.now(),
+					timestamp: Date.now(),
+				});
+			}
+			return users.get(id);
+		},
+		{
+			params: t.Object({
+				id: t.String(),
+			}),
+		}
+	)
+	.get('/epoch', () => currentEpoch)
+	.post('/epoch/tick', () => ++currentEpoch)
 	.post('/echo', ({ body }) => body)
-	.onError(({ code }) => {
-		if (code === 'NOT_FOUND') return 'Route not found :(';
-	})
 	.listen(3000);
 
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`Creating sample data...`);
+
+const serverPath = `http://${app.server?.hostname}:${app.server?.port}`;
+app.handle(new Request(`${serverPath}/user/alpha`, { method: 'POST' }));
+app.handle(new Request(`${serverPath}/epoch/tick`, { method: 'POST' }));
+app.handle(new Request(`${serverPath}/user/beta`, { method: 'POST' }));
+app
+	.handle(new Request(`${serverPath}/epoch/tick`, { method: 'POST' }))
+	.then(console.log);
+
+console.log(`🦊 Elysia is running at ${serverPath}`);
